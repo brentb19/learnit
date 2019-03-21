16b3c125c74a63538a5792445485d6e755a8ffc8
Brent Barrilleaux <brentbarrilleaux@gmail.com>
2019-03-21 08:14:28 -0500

    another possible fix for images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,5 +11,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
   config.fog_public     = true
-  config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+  # config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
 end
