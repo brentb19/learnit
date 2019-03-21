f6c24d3717e8a3bb4b629475dcc91b88d1d45083
Brent Barrilleaux <brentbarrilleaux@gmail.com>
2019-03-21 08:53:34 -0500

    image issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,5 +9,6 @@ CarrierWave.configure do |config|
 
   config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
   config.fog_public     = true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
 end
